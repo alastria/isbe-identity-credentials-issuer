c2b215ff670fef32b199e7c95d5c364155531f3d
David Lutzardo <jdlutzardo@izertis.com>
2025-11-27 13:08:34 +0100

    bug in comment for JS

--- .github/workflows/scripts/insert-header-in-code-files.py
+++ .github/workflows/scripts/insert-header-in-code-files.py
@@ -47,7 +47,7 @@ HEADER_JS = """/**
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*"""
+*/"""
 
 
 def insert_header_in_code_file(file_path, header, hearder_first_line):
